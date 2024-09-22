06660334ff54e3970273fda580b3d5ff2dbb195d
lsd <danlewismuriuki2@gmail.com>
2024-09-22 17:21:30 +0300

    feat: implement file reading and response handling
    - Used fs.readFile to read contents of notes.txt asynchronously
    - Added error handling to log any read errors to the console
    - Sent file contents in the HTTP response wrapped in <pre> tags

--- 04-NodeJS-Server-Core-Modules-FS/server.ts
+++ 04-NodeJS-Server-Core-Modules-FS/server.ts
@@ -1,4 +1,6 @@
 import http, { Server, IncomingMessage, ServerResponse } from 'http';
+import path from 'path';
+import fs from 'fs';
 
 const hostname: string = '127.0.0.1';
 const port: number = 5000;
@@ -6,6 +8,13 @@ const port: number = 5000;
 const requestListener = (request: IncomingMessage, response: ServerResponse) => {
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/html');
+
+    fs.readFile(path.join(__dirname, 'data', 'notes.txt'), 'utf-8', (error, result) => {
+        if (error) {
+            console.log(error)
+        }
+        response.end(`<pre>${result}</pre>`)
+    });
 
 
     // response.end(`<h3 style="font-family: Lato, sans-serif; color: green">Welcome to NodeJs`)
